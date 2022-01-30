00449edc3d6c2b45bffd9f360bf434c72566ce20
przem.exe <92759296+przemex94@users.noreply.github.com>
2022-01-30 23:37:12 +0100

    v 1.0

--- scangrid.sh
+++ scangrid.sh
@@ -2,6 +2,7 @@
 #scangrid.sh - A simple nmap scan wrapper.
 
 username=$(whoami | cut -c -6)
+year=$(date +"%Y")
 datestring=$(date +"%d-%m-%Y_%T")
 script="$0"
 initials="$1"
@@ -9,67 +10,114 @@ projectname="$2"
 function="$3"
 targetlist="$4"
 
-# Put executed command to logit.log
-echo $datestring "executed by:" $username "$0" "$@" >> logit.log
-
-
-#FUNCTIONS
-#Buffer with corpkey vs initials array
-username_buffer(){
+#Functions
+#Initials input buffer
+initials_buffer(){
     if [[ ${#initials} -ge 5 ]] ; then echo "Error: Bad initials. Too long!!!" ; break
     elif [[ ${#initials} -le 3 ]] ; then echo "Error: Bad initials. Too short!!!" ; break
-    #elif [ `expr "$initials" : ".*[!@#\$%^\&*()_+].*"` -gt 0 ]; then echo "This str contain sspecial symbol" ; break
+    elif [[ $projectname == *['!'@'#'\$%^\&*()_+]* ]] ; break
     else
-        break
+        :
     fi
+}
+
+#Projectname input buffer
+projectname_buffer(){
+    if [[ $projectname == *['!'@'#'\$%^\&*()_+]* ]] ; break
+    else
+        :
+    fi
+}
+
+# Put executed command to /srv/scangrid/$date/
+logit_buffer() {
+initials_buffer
+projectname_buffer
+    if [ -d "/srv/scangrid/logit.log"]; then
+        :
+    else
+        if [ -d "/srv/scangrid"]; then
+            :
+        else
+            mkdir -p /srv/scangrid
+        fi
+        touch /srv/scangrid/logit.log
+    fi
+echo $datestring "executed by:" $initials $username "$0" "$@" >> /srv/scangrid/logit.log
 }
 
 #Mini tcp scan run
 mini_tcp_scan_run() {
-    echo testnamebuffer
-}
+    logit_buffer
+    initials_buffer
+    projectname_buffer
+    id=`ls /srv/scangrid/ | sort | tail -1`
+    if [ -d "/srv/scangrid/minitcp/$id"]; then
+        id=$((++id))
+    else
+        mkdir -p /srv/scangrid/minitcp/$id
+    fi
+    screen -S $projectname+_minitcp -dm bash -c "nmap -p- -A -oA /srv/scangrid/minitcp/$id --stats-every 1s --script=all --version-all --osscan-guess --unpriviledged"
+    exit 1
+}   
+
 #Tcp scan run
 tcp_scan_run() {
-    username_buffer
-    if [ -d "/home/domain/$username/$projectname" ]; then
-        :
+    logit_buffer
+    initials_buffer
+    projectname_buffer
+    id=`ls /srv/scangrid/ | sort | tail -1`
+    if [ -d "/srv/scangrid/minitcp/$id"]; then
+        id=$((++id))
     else
-        mkdir /home/domain/$username/$projectname
+        mkdir -p /srv/scangrid/tcp/$id
     fi
-    screen -S $projectname -dm bash -c "nmap -p- -A -oA /home/domain/$username/$projectname/nmaptcp --stats-every 1s --script=all --version-all --osscan-guess --unprivileged -Pn -T4 -vvv --reason -iL $targetlist | tee log.log" 
+    screen -S $projectname+_tcp -dm bash -c "nmap -p- -A -oA /srv/scangrid/tcp/$id --stats-every 1s --script=all --version-all --osscan-guess --unprivileged -Pn -T4 -vvv --reason | tee tcplog.log" 
     exit 1
 }
+
 #Udp scan run
 udp_scan_run() {
-    username_buffer
-    if [ -d "/home/domain/$username/$projectname" ]; then
-        :
+    logit_buffer
+    initials_buffer
+    projectname_buffer
+    id=`ls /srv/scangrid/ | sort | tail -1`
+    if [ -d "/srv/scangrid/minitcp/$id"]; then
+        id=$((++id))
     else
-        mkdir /home/domain/$username/$projectname
+        mkdir -p /srv/scangrid/udp/$id
     fi
-    screen -S $projectname -dm_ sh -c "nmap -p- -A -oA /home/domain/$username/$projectname/nmapudp --stats-every 1s --script=all --version-all --osscan-guess --privileged -Pn -T4 -vvv --reason -sU -iL $targetlist | tee sudolog.log"
+    screen -S $projectname -dm sh -c "nmap -p- -A -oA /srv/scangrid/udp/$id --stats-every 1s --script=all --version-all --osscan-guess --privileged -Pn -T4 -vvv --reason -sU | tee udplog.log"
     exit 1
 }
+
 #List of person scan
-list() {
-    username_buffer
-    list=`ps -aux | grep $username | grep 'nmap' | grep -v grep | grep -v './mykmyk' | grep -v 'SCREEN'`
-    echo "$initials $list"
+list_func() {
+    logit_buffer
+    initials_buffer
+    projectname_buffer
+    templst=`ps -aux | grep $username | grep 'nmap' | grep -v grep | grep -v './mykmyk' | grep -v 'SCREEN'`
+    echo "$initials $templst"
     exit 1
 }
+
 #Scan finder
-find() {
-    username_buffer
+find_func() {
+    initials_buffer
+    projectname_buffer
     finder=`ps -aux | grep nmap | grep $username | grep $projectname | grep -v grep | grep -v 'SCREEN' | grep -v './mykmyk' | grep -v tee`
     echo "$finder"
     exit 1
 }
+
 #Scan results
-results() {
-    username_buffer
+results_func() {
+    initials_buffer
+    projectname_buffer
     echo $testname && tail -2 log.log | grep -v grep | grep "Timing: About" | xargs -n 1 | grep %
     exit 1
 }
+
 #Help Banner
 display_help() {
     echo -e "\n"
@@ -95,56 +143,89 @@ display_help() {
     exit 0
 }
 
-#Loop loop / whoop whoop!
+#Loop loop whoop whoop! 
 while :
 do
 case $1 in
     -h | --help |?)
+        logit_buffer
         display_help
         ;;
     -* | --*)
+        logit_buffer
         display_help
         ;;
-    esac
+    *)  logit_buffer
+        break
+        ;;
+esac
 case $2 in
     -l | --list)
-        list
+        logit_buffer
+        list_funct
         ;;
     -* | --*)
+        logit_buffer
         display_help
-        shift 2
+        exit 0
         ;;
-    esac
+    *)  logit_buffer
+        break
+        ;;        
+esac
 case $3 in
     -m | --mtcp)
+        initials_buffer
+        logit_buffer
+        username_buffer
+        projectname_buffer
         mini_tcp_scan_run
-        shift 2
+        exit 0
         ;;
     -t | --tcp)
+        initials_buffer
+        logit_buffer
+        username_buffer
+        projectname_buffer
         tcp_scan_run
-        shift 2
+        exit 0
         ;;
     -u | --udp)
+        initials_buffer
+        logit_buffer
+        username_buffer
+        projectname_buffer
         udp_scan_run
-        shift 2
+        exit 0
         ;;
     -f | --find)
+        initials_buffer
+        logit_buffer
+        username_buffer
+        projectname_buffer
         find
-        shift 2
+        exit 0
         ;;
     -r | --results)
+        initials_buffer
+        logit_buffer
+        username_buffer
+        projectname_buffer
         results
-        shift 2
+        exit 0
         ;;
         --)
+        logit_buffer
         display_help
         exit 0
         ;;
-    -* | --*) 
+    -* | --*)
+        logit_buffer
         display_help
         exit 0
         ;;
-    *)  break
+    *)  logit_buffer
+        break
         ;;
-    esac
+esac
 done
